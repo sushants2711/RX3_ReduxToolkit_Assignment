33dfbd8d03156411eb2206c217c899d5f8fedb68
Sushant Singh <sushantsingh@Sushants-MacBook-Air.local>
2025-11-12 12:35:57 +0530

    changes done

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const { initializeDatabase } = require("./config/db.connect");
 const { Student } = require("./model/student.model");
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 initializeDatabase();
